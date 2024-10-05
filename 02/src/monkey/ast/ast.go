034cdbc94604cd3cd0845a8951a2af28d414959e
Jack <101360909+mahj79@users.noreply.github.com>
2024-10-05 17:46:42 -0500

    added 3 function string methods LetStatement, ReturnStatement, and ExpressionStatement

--- 02/src/monkey/ast/ast.go
+++ 02/src/monkey/ast/ast.go
@@ -29,6 +29,12 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name *Identifier
+	Value Expression
+}
+
 type ExpressionStatement struct {
 	Token token.Token
 	Expression Expression
@@ -58,11 +64,6 @@ func (p *Program) String() string {
 	return out.String()
 }
 
-type LetStatement struct {
-	Token token.Token // the token.LET token
-	Name *Identifier
-	Value Expression
-}
 
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
@@ -74,3 +75,39 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	
+	out.WriteString(";")
+ 	
+	return out.String()
+ }
+
+ func (rs *ReturnStatement) String() string {
+ 	var out bytes.Buffer
+ 	
+	out.WriteString(rs.TokenLiteral() + " ")
+ 	if rs.ReturnValue != nil {
+ 		out.WriteString(rs.ReturnValue.String())
+ 	}
+ 
+	out.WriteString(";")
+ 
+	return out.String()
+ }
+
+ func (es *ExpressionStatement) String() string {
+ 	if es.Expression != nil {
+ 		return es.Expression.String()
+ 	}
+ 	return ""
+ }
